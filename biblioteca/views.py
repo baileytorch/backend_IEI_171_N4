75b0b84573da954f000bc31a7e3425b8f9292926
baileytorch <baileytorch@hotmail.com>
2025-09-10 18:23:49 -0300

    Vistas Basadas en Clases

--- biblioteca/views.py
+++ biblioteca/views.py
@@ -1,3 +1,56 @@
 from django.shortcuts import render
+from rest_framework import viewsets
+from .serializer import Nacionalidad_Serializer, Autor_Serializer, Comuna_Serializer, Direccion_Serializer, Biblioteca_Serializer, Libro_Serializer, TipoCategoria_Serializer, Categoria_Serializer, Lector_Serializer, Prestamo_Serializer
+from .models import Nacionalidad, Autor, Comuna, Direccion, Biblioteca, Lector, TipoCategoria, Categoria, Libro, Prestamo
 
 # Create your views here.
+
+
+class Nacionalidad_ViewSet(viewsets.ModelViewSet):
+    queryset = Nacionalidad.objects.all()
+    serializer_class = Nacionalidad_Serializer
+
+
+class Autor_ViewSet(viewsets.ModelViewSet):
+    queryset = Autor.objects.all()
+    serializer_class = Autor_Serializer
+
+
+class Comuna_ViewSet(viewsets.ModelViewSet):
+    queryset = Comuna.objects.all()
+    serializer_class = Comuna_Serializer
+
+
+class Direccion_ViewSet(viewsets.ModelViewSet):
+    queryset = Direccion.objects.all()
+    serializer_class = Direccion_Serializer
+
+
+class Biblioteca_ViewSet(viewsets.ModelViewSet):
+    queryset = Biblioteca.objects.all()
+    serializer_class = Biblioteca_Serializer
+
+
+class Lector_ViewSet(viewsets.ModelViewSet):
+    queryset = Lector.objects.all()
+    serializer_class = Lector_Serializer
+
+
+class TipoCategoria_ViewSet(viewsets.ModelViewSet):
+    queryset = TipoCategoria.objects.all()
+    serializer_class = TipoCategoria_Serializer
+
+
+class Categoria_ViewSet(viewsets.ModelViewSet):
+    queryset = Categoria.objects.all()
+    serializer_class = Categoria_Serializer
+
+
+class Libro_ViewSet(viewsets.ModelViewSet):
+    queryset = Libro.objects.all()
+    serializer_class = Libro_Serializer
+
+
+class Prestamo_ViewSet(viewsets.ModelViewSet):
+    queryset = Prestamo.objects.all()
+    serializer_class = Prestamo_Serializer
